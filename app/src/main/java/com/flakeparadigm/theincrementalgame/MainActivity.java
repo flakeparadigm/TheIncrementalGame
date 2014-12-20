fa212c18af1045951acf138517c2349747ccf683
Tyler <flakeparadigm@gmail.com>
2014-12-20 00:16:27 -0700

    v0.2 : Auto click added Added auto click to the android app!

--- app/src/main/java/com/flakeparadigm/theincrementalgame/MainActivity.java
+++ app/src/main/java/com/flakeparadigm/theincrementalgame/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -15,16 +17,24 @@ public class MainActivity extends Activity {
     private int clickCount = 0;
     private int incrementFactor = 1;
     private TextView countView;
+    private Button autoButton;
     private AlertDialog quitDialog;
+
+    // autoincrementing
+    private Handler h = new Handler();
+    int delay = 1000; //milliseconds
+    boolean doIncrement = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // start counter
         countView = (TextView) findViewById(R.id.click_count);
         updateCount();
 
+        // create quit dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(R.string.quit_message);
         builder.setTitle(R.string.quit_title);
@@ -34,8 +44,10 @@ public class MainActivity extends Activity {
                 decrement();
             }
         });
-
         quitDialog = builder.create();
+
+        // get auto increment button
+        autoButton = (Button) findViewById(R.id.auto_button);
     }
 
 
@@ -52,6 +64,26 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void autoIncrementToggle(View view) {
+        if(doIncrement) {
+            autoButton.setText(R.string.auto_click_off);
+            doIncrement = false;
+
+        } else {
+            // create auto incrementer
+            h.postDelayed(new Runnable(){
+                public void run(){
+                    if( doIncrement ) {
+                        increment(null);
+                        h.postDelayed(this, delay);
+                    }
+                }
+            }, delay);
+
+            autoButton.setText(R.string.auto_click_on);
+            doIncrement = true;
+        }
+    }
     public void increment(View view) {
         clickCount += incrementFactor;
         updateCount();
